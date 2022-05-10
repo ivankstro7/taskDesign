674a3e48ad5f716977f603ae41e95028b5978035
ivankstro7 <ivankstro7@gmail.com>
2022-05-10 16:22:48 -0500

    add localStorage

--- src/components/TodoList/task.js
+++ src/components/TodoList/task.js
@@ -5,14 +5,63 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import './task.css';
 
-const defaultTodos  = [
-  { text: 'Take course intro to react.js', completed: false },
-  { text: 'pass everything to a style sheet in Heredium-App', completed: false },
-  { text: 'Buy marriage shoes', completed: false },
-];
+// const defaultTodos  = [
+//   { text: 'Take course intro to react.js', completed: false },
+//   { text: 'pass everything to a style sheet in Heredium-App', completed: false },
+//   { text: 'Buy marriage shoes', completed: false },
+// ];
+
+function useLocalStorage(itemName, initialValue) {
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [item, setItem] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+        
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
+
+        setItem(parsedItem);
+        setLoading(false);
+      } catch(error) {
+        setError(error);
+      }
+    }, 1000);
+  });
+
+  const saveItem = (newItem) => {
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch(error) {
+      setError(error);
+    }
+  };
+
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
+}
 
 function TaskContainer() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -34,14 +83,14 @@ function TaskContainer() {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex,1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
@@ -56,8 +105,14 @@ function TaskContainer() {
       />
 
       <TodoList>
+        {error && <p>Desespérate, hubo un error...</p>}
+        {loading && <p>Estamos cargando, no desesperes...</p>}
+        {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
         {todos.map(todo => (
           <TodoItem 
+            loading={loading}
+            error={error}
+            totalTodos={totalTodos}
             key={todo.text} 
             text={todo.text} 
             user='ivan castro' 
@@ -69,6 +124,6 @@ function TaskContainer() {
       </TodoList>
     </div>
   );
-  }
+}
   
-  export { TaskContainer };
+export { TaskContainer };
